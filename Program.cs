6ffb550ba82a535e5c1a7b28b73a8a4710b16b90
polvonovsadriddin <144240322+polvonovsadriddin@users.noreply.github.com>
2023-11-18 20:46:34 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿//вариант 1 высокий уравень
+Console.WriteLine( "|     x  |     y  |");
+for (double x = 0; x<=5; x+=0.25)
+{ 
+    double u = 0;
+    double y = 0;
+    if (x <= 2)
+    {
+        for (int k = 1; k < 5; k++)
+        {
+            u = k * x * x + k - x;
+
+        }
+    }
+    else u = Math.Atan(x);
+   
+
+   
+    y += 1.5 * Math.Abs(Math.Sin(u));
+    Console.WriteLine($"|{x,8:F2}|{y,8:F2}|");
+}
+
